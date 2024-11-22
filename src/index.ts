44e083a3251db0ddfecf0c913b920112523cb5ae
WangZhenghai <fisschl@163.com>
2024-11-22 17:00:14 +0800

    出现构建异常。暂停构建过程

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { useRoute } from "vue-router";
 import { reactiveComputed } from "@vueuse/core";
-import { unset } from "lodash-es";
+import { debounce } from "lodash-es";
+import { useRoute } from "vue-router";
 
 /**
  * 生成 URLSearchParams 对象
  */
 export const toSearchParams = (
   data: object | null | undefined,
-  searchParams = new URLSearchParams()
+  searchParams = new URLSearchParams(),
 ) => {
   if (!data) return searchParams;
   for (const [key, value] of Object.entries(data)) {
@@ -31,6 +31,22 @@ export const recordQuery = (query: unknown) => {
   return result;
 };
 
+export { debounce };
+
+/**
+ * 删除对象中的属性
+ */
+export const unset = (data: any, key: string) => {
+  delete data[key];
+};
+
+/**
+ * 清空一个对象
+ */
+export const clearObject = <T extends object>(data: T) => {
+  Object.keys(data).forEach((key) => unset(data, key));
+};
+
 /**
  * 从当前路由中提取所有字符串类型的查询参数。
  */
@@ -39,11 +55,4 @@ export const useRouteQuery = () => {
   return reactiveComputed(() => {
     return recordQuery(route.query);
   });
-};
-
-/**
- * 清空一个对象
- */
-export const clearObject = <T extends object>(data: T) => {
-  Object.keys(data).forEach((key) => unset(data, key));
 };
