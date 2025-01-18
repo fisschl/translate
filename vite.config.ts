907181f133a4aa5253788a1ecb4282741e30bf9b
WangZhenghai <fisschl@163.com>
2025-01-18 19:25:51 +0800

    更改为 React 客户端渲染

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import VueRouter from "unplugin-vue-router/vite";
+import cssnano from "cssnano";
+import legacy from "@vitejs/plugin-legacy";
+import tailwindcss from "tailwindcss";
+import tailwindNesting from "tailwindcss/nesting";
+import postcssPresetEnv from "postcss-preset-env";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   base: "/translate/",
-  plugins: [VueRouter({}), vue()],
+  plugins: [
+    react(),
+    legacy({
+      modernPolyfills: true,
+    }),
+  ],
+  build: {
+    cssMinify: false,
+  },
+  css: {
+    devSourcemap: true,
+    transformer: "postcss",
+    postcss: {
+      plugins: [
+        tailwindNesting(),
+        tailwindcss(),
+        postcssPresetEnv(),
+        cssnano(),
+      ],
+    },
+  },
 });
